77e291593152988b63ea89c7a1c8cd1afcae6582
Junaid Muzammil <junaid@ubuntu.(none)>
2015-11-16 00:20:54 +0500

    Added Public Transport Regression Analysis

--- regression.R
+++ regression.R
@@ -4,6 +4,9 @@ data = read.csv('Westminster.csv', header=TRUE)
 dataByCP = subset(data, CP==cp)
 years = dataByCP[,1]
 numberOfVehicles = dataByCP[,24]
+busesCoaches = dataByCP[,15]
+
+#Linear Regression
 lse = lm(numberOfVehicles~years)
 yfit = lse$fitted.values
 png('RegressionPlot.png')
@@ -13,3 +16,15 @@ lines(years, yfit)
 newdata = expand.grid(years=c(predict_yr))
 predict_val = predict(lse, newdata)
 cat(paste(c("Predicted Value for year", predict_yr, "is", predict_val, "\n"), collapse=" "))
+
+
+#Analyzing for Bus Data
+lse = lm(busesCoaches~years)
+yfit = lse$fitted.values
+png('RegressionPlotForPublicTransport.png')
+plot(years, busesCoaches)
+lines(years, yfit)
+
+newdata = expand.grid(years=c(predict_yr))
+predict_val = predict(lse, newdata)
+cat(paste(c("Predicted Value of Public Transport for year", predict_yr, "is", predict_val, "\n"), collapse=" "))

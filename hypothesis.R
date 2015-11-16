5d9dce99964a45bf492c661a05f22840c47d0bd9
Junaid Muzammil <junaid@ubuntu.(none)>
2015-11-16 21:03:58 +0500

    Hypothesis test

--- hypothesis.R
+++ hypothesis.R
@@ -1,6 +1,8 @@
+
+
 cp = '6119'
-claim = 2000
-alpha = 0.40
+claim = 1800
+alpha = 0.975
 data = read.csv('Westminster.csv', header=TRUE)
 dataByCP = subset(data, CP==cp)
 years = dataByCP[,1]
@@ -9,10 +11,18 @@ sampleSize = length(numberOfVehicles)
 meanVehicles = mean(numberOfVehicles)
 sdVehicles = sd(numberOfVehicles)* sqrt((sampleSize-1)/(sampleSize))
 t_val = (meanVehicles - claim)/(sdVehicles/sqrt(sampleSize))
+t_val = abs(t_val)
+cat("t calculated value ")
+cat(t_val)
+cat("\n")
 acceptance_region = 1-alpha
 df = sampleSize-1
-t.alpha = qt(acceptance_region, df=df)
-if(t_val > -t.alpha && t_val < t.alpha) {
+t_alpha = qt(acceptance_region, df=df)
+t_alpha = abs(t_alpha)
+cat("t critical value ")
+cat(t_alpha)
+cat("\n")
+if(t_val > -t_alpha && t_val < t_alpha) {
     cat ("Null Hypothesis accepted and alternate hypothesis rejected\n")
 } else {
     cat ("Null Hypothesis rejected and alternate hypothesis accepted\n")
